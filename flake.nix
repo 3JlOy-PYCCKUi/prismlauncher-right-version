6c31353e48b391fa75b8d9413adffe5ca5765cab
3JlOy_PYCCKUI <3jl0y_pycckui@riseup.net>
2023-06-14 18:41:09 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "prismlauncher-right-version for nix and nixos";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @{ self, flake-utils, nixpkgs, ... }:
+    let
+      overlays = [
+        # Other overlays
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        packages = {
+          prismlauncher-right-version = pkgs.prismlauncher.overrideAttrs (old: {
+            patches =
+              old.patches or [ ] ++
+              [
+                ./0001.patch
+              ];
+          });
+        };
+        defaultPackage = packages.prismlauncher-right-version;
+      });
+}
+
